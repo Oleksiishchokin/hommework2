1fef7002638ddd362a6678ec88cdf0a2e5ffe376
Oleksiishchokin <124196882+Oleksiishchokin@users.noreply.github.com>
2023-02-11 14:59:28 +0200

    Add files via upload

---
+++ primitivetypes.java
@@ -0,0 +1,26 @@
+package hommework2;
+
+public class primitivetypes {
+    public static void main(String[] args) {
+
+      char ch = 'G';
+
+      int in = 89;
+      byte by = 4;
+      short sc = 56;
+
+      float fl = 4.7333436F;
+      double du = 4.355453532;
+
+      long lo = 12121L;
+
+      System.out.println(ch);
+      System.out.println(in);
+      System.out.println(by);
+      System.out.println(sc);
+      System.out.println(fl);
+      System.out.println(du);
+      System.out.println(lo);
+
+    }
+}
